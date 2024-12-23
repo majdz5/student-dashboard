c00b892b29fdf4921194444414b5437b3e54e13b
Majdz <majd2005zeid@gmail.com>
2024-12-23 23:54:56 +0200

    Changed layout, sidebar in progress

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 const dateElement = document.getElementById('date');
 const today = new Date();
 dateElement.textContent = today.toLocaleDateString();
+
+const floatingBtn = document.querySelector('.floating-btn');
+const sidebar = document.querySelector('.sidebar');
+const plussign = document.querySelector('.plus-icon');
+
+floatingBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+  floatingBtn.classList.toggle('open');
+  plussign.classList.toggle('clicked');
+
+});
