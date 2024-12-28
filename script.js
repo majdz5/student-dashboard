6bb598fb6a0ee60b85f36cd6e43868dd5caaf29c
Majd.z <majd2005zeid@gmail.com>
2024-12-28 22:56:33 +0200

    Removed Top Section/ Completed Side bar

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-// Display current date
-const dateElement = document.getElementById('date');
-const today = new Date();
-dateElement.textContent = today.toLocaleDateString();
+const shrink_btn = document.querySelector(".shrink-btn");
+const search = document.querySelector(".search");
+const sidebar_links = document.querySelectorAll(".sidebar-links a");
+const active_tab = document.querySelector(".active-tab");
+const shortcuts = document.querySelector(".sidebar-links h4");
+const tooltip_elements = document.querySelectorAll(".tooltip-element");
 
-const floatingBtn = document.querySelector('.floating-btn');
-const sidebar = document.querySelector('.sidebar');
-const plussign = document.querySelector('.plus-icon');
+let activeIndex;
 
-floatingBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('open');
-  floatingBtn.classList.toggle('open');
-  plussign.classList.toggle('clicked');
+shrink_btn.addEventListener("click", () => {
+  document.body.classList.toggle("shrink");
+  setTimeout(moveActiveTab, 400);
 
+  shrink_btn.classList.add("hovered");
+
+  setTimeout(() => {
+    shrink_btn.classList.remove("hovered");
+  }, 500);
+});
+
+search.addEventListener("click", () => {
+  document.body.classList.remove("shrink");
+  search.lastElementChild.focus();
+});
+
+function changeLink() {
+  sidebar_links.forEach((sideLink) => sideLink.classList.remove("active"));
+  this.classList.add("active");
+
+  activeIndex = this.dataset.active;
+
+  moveActiveTab();
+}
+
+sidebar_links.forEach((link) => link.addEventListener("click", changeLink));
+
+function showTooltip() {
+  let tooltip = this.parentNode.lastElementChild;
+  let spans = tooltip.children;
+  let tooltipIndex = this.dataset.tooltip;
+
+  Array.from(spans).forEach((sp) => sp.classList.remove("show"));
+  spans[tooltipIndex].classList.add("show");
+
+  tooltip.style.top = `${(100 / (spans.length * 2)) * (tooltipIndex * 2 + 1)}%`;
+}
+
+tooltip_elements.forEach((elem) => {
+  elem.addEventListener("mouseover", showTooltip);
 });
